00e9a398cdc9c213279e4a520010bc1b6ba741c5
Vu Manh Huu <vuhuu2k4@gmail.com>
2024-05-31 23:55:00 +0700

    Chạy thử code phần sản phẩm

---
+++ testcodeProduct.cpp
@@ -0,0 +1,213 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+struct Product {
+    int id;
+    string name;
+    double price;
+    int quantity;
+};
+
+struct Customer {
+    string phone;
+    string name;
+    long point;
+};
+
+struct NodeProduct {
+    Product product;
+    NodeProduct* next;
+};
+
+typedef NodeProduct* Storage;
+
+struct NodeCustomer {
+    Customer customer;
+    NodeCustomer* next;
+};
+
+typedef NodeCustomer* CustomerList;
+
+class ProductManager {
+private:
+    Storage S;
+    int sizeItem;
+public:
+    ProductManager() : S(NULL), sizeItem(0) {}
+    void addProduct();
+    int getSizeItem() const { return sizeItem; }
+    NodeProduct* findProductById(int id);
+    void updateProduct();
+    void deleteProduct();
+    void displayProducts() const;
+    void filter();
+};
+
+void ProductManager::addProduct() {
+    bool check;
+    char c;
+    do {
+        NodeProduct* P = new NodeProduct;
+        cout << "ID: ";
+        cin >> P->product.id;
+        cin.ignore(); 
+        cout << "Ten san pham: ";
+        getline(cin, P->product.name);
+        cout << "Gia ban: ";
+        cin >> P->product.price;
+        cout << "So luong: ";
+        cin >> P->product.quantity;
+        P->next = NULL;
+
+        if (S == NULL) {
+            S = P;
+        } else {
+            NodeProduct* R = S;
+            while (R->next != NULL) R = R->next;
+            R->next = P;
+        }
+        sizeItem++;
+
+        cout << "Tiep tuc them san pham? (y/n): ";
+        cin >> c;
+        if (c == 'y') check = true; 
+        else check = false;
+        cin.ignore();
+    } while (check);
+}
+
+NodeProduct* ProductManager::findProductById(int id) {
+    NodeProduct* P;
+    P = S;
+    while(P!=NULL) {
+        if (P->product.id==id) return P;
+        else P = P->next;
+    }
+    return NULL;
+}
+
+void ProductManager::updateProduct() {
+    char check;
+    do {
+        int id;
+        cout << "Nhap ID san pham can cap nhat: ";
+        cin >> id;
+        cin.ignore();
+        NodeProduct* Q;
+        Q = findProductById(id);
+        if (Q==NULL) {
+            cout << "Khong tim thay san pham!" << endl;
+        } else {
+            cout << "Ten san pham: ";
+            getline(cin, Q->product.name);
+            cout << "Gia ban: ";
+            cin >> Q->product.price;
+            cout << "So luong: ";
+            cin >> Q->product.quantity;
+        }
+        cout << "Tiep tuc cap nhat? (y/n): ";
+        cin >> check;
+    } while (check =='y');
+}
+void ProductManager::deleteProduct() {
+    cout << "Nhan ID san pham can xoa: ";
+    int id;
+    cin >> id;
+    cin.ignore();
+    NodeProduct* Q;
+    Q = findProductById(id);
+    NodeProduct* P = Q;
+    if (Q==NULL) cout << "Khong tim thay san pham!";
+    if (S==Q && Q->next==NULL) {
+        S = NULL;
+        delete Q;
+    } else {
+        if (S==Q) {
+            S = Q->next;
+            delete Q;
+        } else {
+            NodeProduct* R = S;
+            while (R->next!=Q) R = R->next;
+            R->next = Q->next;
+            delete Q;
+        }
+        sizeItem--;
+        cout << "Da xoa san pham:  " << P->product.name << endl;
+    } 
+
+}
+
+void ProductManager::displayProducts() const {
+    cout << "So loai mat hang:  " << getSizeItem() << endl;
+    NodeProduct* P = S;
+    cout << left << setw(10) << "ID" << setw(25) << "Ten san pham" << setw(10) << "Gia ban" << setw(10) << "So luong" << endl;
+    cout << left << setw(10) << "--------" << setw(25) << "-----------------------" << setw(10) << "--------" << setw(10) << "--------" << endl;
+    while(P!=NULL) {
+        cout << left << setw(10) << P->product.id << setw(25) << P->product.name << setw(10) << P->product.price << setw(10) << P->product.quantity << endl;
+        P = P->next;
+    }
+};
+// Loc tim kiem (id, ten, khoang gia)
+//id: chi 1 
+//ten: tu goi y
+//khoang gia: min - max
+void ProductManager::filter() {
+    cout << "1. ID\n2. Ten san pham\n3. Khoang gia\n";
+    int a, id;
+    cout << "Ban chon: ";
+    cin >> a;
+    char check;
+    string tensp;
+    switch (a)
+    {
+    case 1:
+        do {
+            cout << "Nhap ID: ";
+            cin >> id;
+            cin.ignore();
+            NodeProduct* P;
+            P = findProductById(id);
+            if (P==NULL) {
+                cout << "Khong tim thay san pham!" << endl;
+            } else {
+                cout << left << setw(10) << "ID" << setw(25) << "Ten san pham" << setw(10) << "Gia ban" << setw(10) << "So luong" << endl;
+                cout << left << setw(10) << "--------" << setw(25) << "-----------------------" << setw(10) << "--------" << setw(10) << "--------" << endl;
+                cout << left << setw(10) << P->product.id << setw(25) << P->product.name << setw(10) << P->product.price << setw(10) << P->product.quantity << endl;
+            }
+            cout << "Tiep tuc tim kiem? (y/n): ";
+            cin >> check;
+        } while (check =='y');
+        break;
+    case 2:
+        cout << "Nhap ten san pham: ";
+        cin.ignore();
+        getline(cin, tensp);
+        NodeProduct* P;
+        P = S;
+        cout << left << setw(10) << "ID" << setw(25) << "Ten san pham" << setw(10) << "Gia ban" << setw(10) << "So luong" << endl;
+        cout << left << setw(10) << "--------" << setw(25) << "-----------------------" << setw(10) << "--------" << setw(10) << "--------" << endl;
+        while(P!=NULL) {
+        if (P->product.name.find(tensp)!=string::npos) {
+            cout << left << setw(10) << P->product.id << setw(25) << P->product.name << setw(10) << P->product.price << setw(10) << P->product.quantity << endl;
+        }
+        P = P->next;
+        }
+        break;
+    case 3:
+        break;
+    default:
+        cout << "Khong hop le, vui long thu lai!";
+        break;
+    }
+}
+int main() {
+    ProductManager productManager;
+    productManager.addProduct();
+    productManager.updateProduct();
+    productManager.deleteProduct();
+    productManager.displayProducts();
+    productManager.filter();
+    return 0;
+}
